c47ae375a7ea3332d91bfab7f74ba0a3f916bea5
Asweh Debrej <18221145@std.stei.itb.ac.id>
2023-12-12 16:58:58 +0700

    init shield

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'aswehdebrej@gmail.com';
+    public string $fromName   = 'Asweh Debrej';
     public string $recipients = '';
 
     /**
