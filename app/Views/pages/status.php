0fe3ff5831be30f2064ebefbe4ea9473b9f42217
Adrenalin Apprizal <18221125@std.stei.itb.ac.id>
2023-12-17 00:06:19 +0700

    update status

--- app/Views/pages/status.php
+++ app/Views/pages/status.php
@@ -11,41 +11,6 @@
             <thead>
               <tr>
                 <th scope="col">ID</th>
-                <th scope="col">Date Created</th>
-                <th scope="col">Paid Amount</th>
-                <th scope="col">Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php foreach ($order as $o) : ?>
-                <tr>
-                  <td><?= $o['id']; ?></td>
-                  <td><?= $o['name']; ?></td>
-                  <td>Rp.<?= $o['total_price']; ?></td>
-                  <td><?= $o['status']; ?></td>
-                </tr>
-              <?php endforeach; ?>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<?= $this->endSection(); ?><?= $this->extend('layout/template'); ?>
-
-<?= $this->section('content'); ?>
-<div class="container">
-  <div class="row">
-    <div class="col">
-      <div class="container mt-5 mb-4">
-        <h1><strong>Your Orders</strong></h1>
-        <div class="container mt-5">
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
                 <th scope="col">Last Updated</th>
                 <th scope="col">Address</th>
                 <th scope="col">Paid Amount</th>
@@ -54,7 +19,7 @@
             </thead>
             <tbody>
               <?php foreach ($order as $o) : ?>
-                <?php if ($o['id'] == session()->get('user_id')) : ?>
+                <?php if ($o['id'] == session()->get('id')) : ?>
                   <tr>
                     <td><?= $o['id']; ?></td>
                     <td><?= $o['updated_at']; ?></td>
