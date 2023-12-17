154c609e8d42af148ea9126cdc1fb5ff4c52d187
Asweh Debrej <18221145@std.stei.itb.ac.id>
2023-12-17 15:51:41 +0700

    aliasing routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,11 +5,12 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Drink::index');
-$routes->get('/checkout', 'Checkout::index');
-$routes->post('/checkout/saveOrder', 'Checkout::saveOrder');
-$routes->get('/status', 'Status::index');
-$routes->post('/drink/addToCheckout', 'Drink::addToCheckout');
+$routes->get('/drink', 'Drink::index', ['as' => 'drink']);
+$routes->addRedirect('/', 'drink');
+$routes->get('/checkout', 'Checkout::index', ['as' => 'checkout']);
+$routes->post('/checkout/saveOrder', 'Checkout::saveOrder', ['as' => 'saveOrder']);
+$routes->get('/status', 'Status::index', ['as' => 'status']);
+$routes->post('/drink/addToCheckout', 'Drink::addToCheckout', ['as' => 'addToCheckout']);
 
 
 
