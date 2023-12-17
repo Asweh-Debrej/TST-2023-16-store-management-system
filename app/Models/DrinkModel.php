ba4c6b8f9d444a94d41df92318a8570d081f426b
Asweh Debrej <18221145@std.stei.itb.ac.id>
2023-12-17 17:12:02 +0700

    Update DrinkModel.php

--- app/Models/DrinkModel.php
+++ app/Models/DrinkModel.php
@@ -8,4 +8,19 @@ class DrinkModel extends Model
 {
   protected $table = 'drinks';
   protected $useTimestamps = true;
+  protected $allowedFields = ['produk', 'harga', 'gambar'];
+
+  public function getDrink($id = false)
+  {
+    if ($id == false) {
+      return $this->findAll();
+    }
+
+    return $this->where(['id' => $id])->first();
+  }
+
+  public function search($keyword)
+  {
+    return $this->table('drinks')->like('produk', $keyword)->orLike('harga', $keyword);
+  }
 }
