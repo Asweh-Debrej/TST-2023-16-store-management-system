1874927cd837dff21e692fe9e89d2814032dd1f2
Asweh Debrej <18221145@std.stei.itb.ac.id>
2023-12-19 01:54:32 +0700

    Update status.php

--- app/Views/pages/status.php
+++ app/Views/pages/status.php
@@ -5,29 +5,26 @@
   <div class="row">
     <div class="col">
       <div class="container mt-5 mb-4">
-        <h1><strong>Your Orders</strong></h1>
         <div class="container mt-5">
           <table class="table">
             <thead>
               <tr>
                 <th scope="col">ID</th>
                 <th scope="col">Last Updated</th>
-                <th scope="col">Address</th>
+                <th scope="col">Destination</th>
                 <th scope="col">Paid Amount</th>
                 <th scope="col">Status</th>
               </tr>
             </thead>
             <tbody>
               <?php foreach ($order as $o) : ?>
-                <?php if ($o['id'] == session()->get('id')) : ?>
-                  <tr>
-                    <td><?= $o['id']; ?></td>
-                    <td><?= $o['updated_at']; ?></td>
-                    <td><?= $o['address']; ?></td>
-                    <td>Rp.<?= $o['total_price']; ?></td>
-                    <td><?= $o['status']; ?></span> </td>
-                  </tr>
-                <?php endif; ?>
+                <tr>
+                  <td><?= $o['id']; ?></td>
+                  <td><?= $o['updated_at']; ?></td>
+                  <td><?= $o['address']; ?></td>
+                  <td>Rp.<?= $o['total_price']; ?></td>
+                  <td><?= $o['status']; ?></span> </td>
+                </tr>
               <?php endforeach; ?>
             </tbody>
           </table>
