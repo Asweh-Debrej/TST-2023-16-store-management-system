0946f20185f861e247e74ecc0b6650c428d23d0e
Asweh Debrej <18221145@std.stei.itb.ac.id>
2023-12-19 07:00:44 +0700

    Update Status.php

--- app/Controllers/Status.php
+++ app/Controllers/Status.php
@@ -17,10 +17,47 @@ class Status extends BaseController
             'timeout' => 5,
         ];
         $this->client = \Config\Services::curlrequest($options);
+        $this->client->setHeader('Accept', 'application/json');
     }
     public function index()
     {
         $orders = $this->orderModel->findAll();
+
+        foreach ($orders as $key => $o) {
+            $response = $this->client->get(getenv('api_delivery_baseUrl') .  '/order/' . $o['delivery_id'], [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer ' . getenv('api_delivery_token'),
+                ],
+            ]);
+
+            if ($response->getStatusCode() === 401) {
+                if (delivery_login()) {
+                    $response = $this->client->get(getenv('api_delivery_baseUrl') .  '/order/' . $o['delivery_id'], [
+                        'headers' => [
+                            'Content-Type' => 'application/json',
+                            'Accept' => 'application/json',
+                            'Authorization' => 'Bearer ' . getenv('api_delivery_token'),
+                        ],
+                    ]);
+                } else {
+                    return redirect()->back()->with('errors', ["we're having trouble connecting to the delivery service"]);
+                }
+            }
+
+            if ($response->getStatusCode() === 404 || $response->getStatusCode() === 403) {
+                continue;
+            } else if ($response->getStatusCode() >= 300) {
+                return redirect()->back()->with('errors', ["unknown error occured"]);
+            }
+
+            $body = $response->getBody();
+            $responseData = json_decode($body, true);
+
+            $orders[$key]['status'] = $responseData['data']['status'];
+            $orders[$key]['estimated_arrival'] = $responseData['data']['estimated_arrival'];
+        }
 
         $data = [
             'title' => 'Your Orders',
